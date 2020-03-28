f2a4264e7a94e12ac657f5dc6ef99af39420c49e
cluelesswalnut <ernst.sukram@gmail.com>
2020-03-28 18:52:11 -0400

    Markus test

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 const originRoomNames: string[] = [`E58S54`];
 import roomOverlord from './roomOverlord';
 
+// Markus
+
 for (let originRoomName of originRoomNames) {
 	const RCL = Game?.rooms[originRoomName]?.controller?.level;
 	roomOverlord(originRoomName, RCL);
