45092419a6586df11d027dd2385f538cd06c6d48
cluelesswalnut <ernst.sukram@gmail.com>
2020-04-05 13:33:53 -0400

    Add rescue protocol for rooms

--- src/main/creep_generation/spawnCreeps.ts
+++ src/main/creep_generation/spawnCreeps.ts
@@ -6,6 +6,11 @@ const spawnCreeps = (originRoomName: string, rolesNeeded: RolesNeeded, RCL: numb
 	const { harvesters, upgraders, miners, builders} = countCreeps(originRoomName);
 
 	const roomSpawners: StructureSpawn[] = _.filter(Game.spawns, x => x.room.name === originRoomName);
+
+	if ( rescueProtocol(originRoomName, roomSpawners, harvesters ) ){
+		return;
+	}
+
 	for (let spawner of roomSpawners) {
 		// if ( rolesNeeded.mine && miners < rolesNeeded.mine) {
 		// 	Game.spawns[spawner.name].spawnCreep(
@@ -46,6 +51,25 @@ const countCreeps = (originRoomName: string) =>{
 
 	// can make the return a RolesNeeded type?
 	return {harvesters, upgraders, miners, builders};
+}
+
+/**
+ * Check if a room needs rescuing and rescues it. Currently just makes sure at least 1 harvester exists
+ * @param originRoomName	Name of the room that is being evaluated
+ * @param roomSpawners		Spawners for the room
+ * @param harvesterCount	Number of harvesters for the room
+ * @returns bool				true if the room needed rescuing, false otherwise
+ */
+const rescueProtocol = (originRoomName: string, roomSpawners: StructureSpawn[], harvesterCount: number) => {
+	const spawner: StructureSpawn = roomSpawners[0];
+	if ( harvesterCount < 1){
+		spawner.spawnCreep(
+			generateBodyParts(`Harvester`, 1),
+			generateName(`Harvester`),
+			generateOptions(originRoomName, `harvester`))
+			return true;
+	}
+	return false;
 }
 
 export default spawnCreeps;
